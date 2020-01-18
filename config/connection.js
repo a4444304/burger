7aa6033ab49bfd370b36908d47d5693d247574d2
unknown <52844667+a4444304@users.noreply.github.com>
2020-01-18 01:52:00 -0600

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,21 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-    host:"xq7t6tasopo9xxbs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user:"cjr91gmta81pos83",
-    password:"r7czsu3b32jqeoix",
-    database:"grjm3a0clpo6i5wn"
-})
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'mysql10',
+        database: 'burgers_db'
+    });
+};
+//  mysql.createConnection({
+//     host:"xq7t6tasopo9xxbs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//     user:"cjr91gmta81pos83",
+//     password:"r7czsu3b32jqeoix",
+//     database:"grjm3a0clpo6i5wn"
+// })
 connection.connect(function(err){
     if(err)throw err;
     console.log("connected as id: " + connection.threadId);
